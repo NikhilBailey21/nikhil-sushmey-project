e37fcf728d28c55ce486fca5ea00e4f5c661781b
NikhilBailey21 <niba3943@colorado.edu>
2025-12-05 12:40:13 -0700

    Updated tests to use root

--- flask-app/tests/conftest.py
+++ flask-app/tests/conftest.py
@@ -1,6 +1,12 @@
 import os
+import sys
 
 import pytest
+
+# Add repo root to Python path to allow importing shared modules
+_repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+if _repo_root not in sys.path:
+    sys.path.insert(0, _repo_root)
 
 try:
     from dotenv import load_dotenv
